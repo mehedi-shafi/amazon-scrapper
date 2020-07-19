216ab1933d277e29e14467019714c128c7987a0a
root <mehedishafi@hotmail.com>
2020-07-19 13:37:49 +0600

    fix: multi thread same file dumping issue

--- multithread.py
+++ multithread.py
@@ -6,14 +6,14 @@ import pandas as pd
 from time import sleep, time
 from fake_useragent import UserAgent
 
-INTERMEDIATE_SLEEP_TIME = 200 # 200ms
+INTERMEDIATE_SLEEP_TIME = 1000 # 200ms
 LONG_GAP_AFTER = 50 # after this # of links scraped system will sleep for a longer period
 LONG_SLEEP_TIMER = 10000 # 10s
 
 BASE_URL = 'https://www.amazon.de/s?me=A3B6Q99ITZLBDT&marketplaceID=A1PA6795UKMFR9'
 PAGINATION_PAGES = 'https://www.amazon.de/s?i=merchant-items&me=A3B6Q99ITZLBDT&page={}&marketplaceID=A1PA6795UKMFR9&qid=1595107300&ref=sr_pg_{}'
 
-SAVE_FILE_PATH = 'data/data.csv'
+SAVE_FILE_PATH = 'data/async_t{}.csv'
 
 AGENTS = UserAgent()
 
@@ -21,13 +21,13 @@ outputDataframe = pd.DataFrame(columns=['url', 'short_code'])
 
 q = queue.Queue()
 
-def saveData(links):
+def saveData(links, threadnumber):
     global outputDataFrame, SAVE_FILE_PATH
     for link in links:
         outputDataframe.loc[len(outputDataframe)] = [link, scraper.getProductId(link)]
-    outputDataframe.to_csv(SAVE_FILE_PATH, index=False)
+    outputDataframe.to_csv(SAVE_FILE_PATH.format(threadnumber), index=False)
 
-def crawler():
+def crawler(threadnumber):
     continueScraping = True
     trial = 0
     currentPage = 1
@@ -37,14 +37,18 @@ def crawler():
         
         if url is None:
             break
-
-        links = scraper.scrape(url, AGENTS.random)
+        
+        links = []
+        try:
+            links = scraper.scrape(url, AGENTS.random)
+        except Exception as E:
+            print(E)
+            print('Error scraping url: {}'.format(url))
 
         if len(links) == 0:
             trial += 1
         else:
             trial = 0
-            saveData(links)
         if trial == 3:
             continueScraping = False
 
@@ -54,6 +58,7 @@ def crawler():
             print('SLEEPING FOR {}'.format(LONG_SLEEP_TIMER))
             sleep(LONG_SLEEP_TIMER / 1000)
 
+        saveData(links, threadnumber)
         sleep(INTERMEDIATE_SLEEP_TIME  / 1000)
         q.task_done()
 
@@ -61,7 +66,7 @@ def startCrawling(numberThread, startNumber, endNumber):
     threads = []
     
     for i in range(numberThread):
-        t = threading.Thread(target=crawler)
+        t = threading.Thread(target=crawler, args=(i+1,))
         t.start()
         threads.append(t)
 
@@ -77,4 +82,4 @@ def startCrawling(numberThread, startNumber, endNumber):
         t.join()
 
 if __name__ == '__main__':
-    startCrawling(5, 4500, 20000)
+    startCrawling(5, 4772, 20000)
